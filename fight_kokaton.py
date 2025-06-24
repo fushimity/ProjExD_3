b19de2343470d933009456c09e6e8d599b746b65
fushimity <fushimity@edu.teu.ac.jp>
2025-06-24 14:31:40 +0900

    練習３：よろこびエフェクト

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -171,6 +171,8 @@ def main():
                 if beam.rct.colliderect(bomb.rct):  # ビームと爆弾が衝突していたら
                     beam = None
                     bomb = None
+                    bird.change_img(6, screen)
+
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
